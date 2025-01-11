8edad09611df880b5c105b73402c8737f5d6cdbf
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2025-01-11 11:59:26 -0800

    chore: add Falcon window size

--- binoculars/__init__.py
+++ binoculars/__init__.py
@@ -1,4 +1,8 @@
-BINOCULARS_ACCURACY_THRESHOLD = 0.9015310749276843
+from typing import Final
+
+BINOCULARS_ACCURACY_THRESHOLD: Final = 0.9015310749276843
 """Score threshold optimized for f1-score, selected using Falcon-7B and Falcon-7B-Instruct at bfloat16"""
-BINOCULARS_FPR_THRESHOLD = 0.8536432310785527
+BINOCULARS_FPR_THRESHOLD: Final = 0.8536432310785527
 """Score threshold optimized for low-fpr (chosen at 0.01%), selected using Falcon-7B and Falcon-7B-Instruct at bfloat16"""
+CONTEXT_WINDOW: Final = 2048
+"""Falcon-7B context window size."""
